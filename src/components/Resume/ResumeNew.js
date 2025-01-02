3e628fc9d04a560e2b969a937214859c9f07a005
Darshan98Solanki <darshan98solanki@gmail.com>
2025-01-02 20:36:44 +0530

    change

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Darshan_Solanki.pdf"
+import pdf from "../../../src/Assets/Darshan_Solanki.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
